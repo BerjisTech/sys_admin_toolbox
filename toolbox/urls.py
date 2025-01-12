fca314b5cb0404d24e68a3729d00f94d10ea6a0c
Benedict <berjistech.gmail.com>
2025-01-12 13:23:29 +0300

    add additional view imports in urls.py for system metrics and logging

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ToolViewSet, ServerViewSet, ServerLogViewSet
+from .views import CategoryViewSet, ToolViewSet, ServerViewSet, ServerLogViewSet, system_metrics, check_uptime, docker_logs
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
